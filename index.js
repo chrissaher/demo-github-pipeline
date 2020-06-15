a88b76ae3cb6f4e33093b14d72c7174ae3b64e7f
chrissaher <chrisssaher@gmail.com>
2020-06-15 17:01:14 +0900

    add basic functionality

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const port = 3000
 
 app.use(express.static(__dirname, {'index': ['index.html', 'login.html']}))
 
-//
-// app.get('./', (req, res)=> {
-//
-// })
+
+app.get('/calculate', (req, res)=> {
+  n1 = parseInt(req.query.n1)
+  n2 = parseInt(req.query.n2)
+  n3 = n1 + n2
+  res.send({'res': n3})
+})
 
 app.listen(port, () => {
   console.log(`listening on port: http://localhost:${port}`)
--- script.js
+++ script.js
@@ -1,2 +1,29 @@
+var displayOperation = (data) => {
+  res = data.res
+  console.log(res)
+  $('#result').val(res)
+}
+
+var displayError = () => {
+  console.log("Unexpected error")
+}
+
+var calculate = (n1, n2) => {
+  $.ajax({
+    url: '/calculate',
+    data: {'n1': n1,'n2': n2},
+  }).done((data) => {
+    displayOperation(data)
+  }).fail(() => {
+    displayError()
+  })
+}
+
 $(() => {
+
+  $('#submit').click(() => {
+    var n1 = $('#n1').val()
+    var n2 = $('#n2').val()
+    calculate(n1, n2)
+	});
 });
