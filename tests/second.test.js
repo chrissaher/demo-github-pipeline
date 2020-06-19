af2de9edd02656405c9be7e46726ce7bfbfedbaf
chrissaher <chrisssaher@gmail.com>
2020-06-19 14:19:12 +0900

    add tests cases

---
+++ tests/second.test.js
@@ -0,0 +1,12 @@
+describe('test suite 2 (?) ', () => {
+  it('Test 1', () => {
+    expect(true).toEqual(false);
+  });
+
+
+  it('Test 2', () => {
+    expect(true).toEqual(true);
+  });
+
+
+});
---
+++ tests/spec.js
@@ -0,0 +1,14 @@
+describe('My Test Suite', () => {
+  it('My Test Case', () => {
+    expect(true).toEqual(true);
+  });
+
+  it('Test 1', () => {
+    expect(true).toEqual(true);
+  });
+
+
+  it('Test 2', () => {
+    expect(true).toEqual(true);
+  });
+});
